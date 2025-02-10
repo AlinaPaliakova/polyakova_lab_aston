9cfba317c4bb5750958b0b714ab024db082d0a62
Alina Polyakova <alina.paliakovaa@gmail.com>
2025-02-10 19:15:24 +0300

    add Lesson 12

---
+++ src/main/java/com/aston/lesson_12/FactorialNumber.java
@@ -0,0 +1,24 @@
+package com.aston.lesson_12;
+
+public class FactorialNumber {
+    long factorial;
+
+    public long getFactorial(int number) {
+        if (number < 0)
+            throw new IllegalArgumentException();
+
+        factorial = 1;
+
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                factorial *= i;
+            }
+        }
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        FactorialNumber factorialNumber = new FactorialNumber();
+        System.out.println(factorialNumber.getFactorial(6));
+    }
+}
---
+++ src/test/java/com/aston/lesson_12/FactorialNumberTest.java
@@ -0,0 +1,40 @@
+package com.aston.lesson_12;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FactorialNumberTest {
+    FactorialNumber factorialNumber;
+
+
+    @BeforeEach
+    public void init() {
+        factorialNumber = new FactorialNumber();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        factorialNumber = null;
+    }
+
+    @Test
+    public void testFactorialNumber() {
+
+        assertEquals(1, factorialNumber.getFactorial(1));
+        assertEquals(2, factorialNumber.getFactorial(2));
+        assertEquals(6, factorialNumber.getFactorial(3));
+        assertEquals(24, factorialNumber.getFactorial(4));
+        assertEquals(120, factorialNumber.getFactorial(5));
+        assertEquals(720, factorialNumber.getFactorial(6));
+        assertEquals(5040, factorialNumber.getFactorial(7));
+        assertEquals(40320, factorialNumber.getFactorial(8));
+        assertEquals(362880, factorialNumber.getFactorial(9));
+        assertEquals(3628800, factorialNumber.getFactorial(10));
+    }
+
+}
