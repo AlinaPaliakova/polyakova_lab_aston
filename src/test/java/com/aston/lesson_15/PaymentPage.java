7649eea4a7b49ff97161016abec5de07ed2f6ef9
Alina Polyakova <alina.paliakovaa@gmail.com>
2025-02-17 21:57:37 +0300

    add Lesson 15

---
+++ src/test/java/com/aston/lesson_15/PaymentPage.java
@@ -0,0 +1,13 @@
+package com.aston.lesson_15;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class PaymentPage {
+    WebDriver driver;
+
+    public PaymentPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
