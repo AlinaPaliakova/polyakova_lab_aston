61157568b8242bab0b0709943d543b0af890e431
Alina Polyakova <alina.paliakovaa@gmail.com>
2025-02-21 09:41:57 +0300

    add Lesson 17

---
+++ src/test/java/com/aston/lesson_17/PutRequestTests.java
@@ -0,0 +1,40 @@
+package com.aston.lesson_17;
+
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.text.MatchesPattern.matchesPattern;
+
+public class PutRequestTests {
+    @Test
+    public void testPutTextResponse() {
+        RestAssured.baseURI = "https://postman-echo.com";
+
+        String requestBody = "This is expected to be sent back as part of response body.";
+
+        given()
+                .contentType("text/plain")
+                .body(requestBody)
+                .when()
+                .put("/put")
+                .then()
+                .statusCode(200)
+                .body("args.isEmpty()", equalTo(true))
+                .body("data", equalTo(requestBody))
+                .body("files.isEmpty()", equalTo(true))
+                .body("form.isEmpty()", equalTo(true))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers['x-request-start']", notNullValue())
+                .body("headers['content-length']", equalTo(String.valueOf(requestBody.length())))
+                .body("headers['x-forwarded-proto']", equalTo("https"))
+                .body("headers['x-forwarded-port']", equalTo("443"))
+                .body("headers.x-amzn-trace-id", startsWith("Root="))
+                .body("headers.accept", equalTo("*/*"))
+                .body("headers['user-agent']", notNullValue())
+                .body("headers.accept-encoding", matchesPattern("gzip\\s*,\\s*deflate"))
+                .body("json", equalTo(null))
+                .body("url", equalTo("https://postman-echo.com/put"));
+    }
+}
